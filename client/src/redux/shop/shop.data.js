cf78d569f34b0292e0e70c535f2be94d3b2b9fcd
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-08-01 09:18:22 +0100

    update shop redux

--- client/src/redux/shop/shop.data.js
+++ client/src/redux/shop/shop.data.js
@@ -1,259 +1,295 @@
-
 const SHOP_DATA = {
-  bedroom: {
-    id: 1,
-    title: 'Bedroom',
-    routeName: 'bedroom',
-    items: [
-      {
-        id: 1,
-        name: 'Aloe Vera',
-        imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
-        price: 25
-      },
-      {
-        id: 2,
-        name: 'Calathea',
-        imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
-        price: 18
-      },
-      {
-        id: 3,
-        name: 'Chinese Money Plant',
-        imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
-        price: 35
-      },
-      {
-        id: 4,
-        name: 'Devils Ivy',
-        imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
-        price: 25
-      },
-      {
-        id: 5,
-        name: 'Peace Lily',
-        imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
-        price: 18
-      },
-      {
-        id: 6,
-        name: 'Snake Plant',
-        imageUrl: 'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',
-        price: 14
-      },
-      {
-        id: 7,
-        name: 'Succulent',
-        imageUrl: 'https://i.ibb.co/bLB646Z/red-beanie.png',
-        price: 18
-      },
-      {
-        id: 8,
-        name: 'Watermelon Peperomia',
-        imageUrl: 'https://i.ibb.co/1f2nWMM/wolf-cap.png',
-        price: 14
-      },
-      {
-        id: 9,
-        name: 'Weeping Fig',
-        imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
-        price: 16
-      }
-    ]
-  },
   bathroom: {
-    id: 2,
-    title: 'Bathroom',
     routeName: 'bathroom',
+    id: 'e9rckoQ14irRrR6BpIdK',
+    title: 'Bathroom',
     items: [
       {
         id: 10,
-        name: 'Anthurium',
-        imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
-        price: 22
+        price: 22,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fbathroom%2Fanthurium.jpg?alt=media&token=67c87aff-ee51-4e59-8e19-01898fcf55dc',
+        name: 'Anthurium'
       },
       {
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fbathroom%2Fchlorophytum-comosum.jpg?alt=media&token=03bfe142-8262-4717-b4d3-f3d15b9b52b8',
         id: 11,
         name: 'Chlorophytum Comosum',
-        imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
         price: 28
       },
       {
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fbathroom%2Fcorn-plant.jpg?alt=media&token=e4bbcd5c-5d95-42c2-9771-b30775785576',
         id: 12,
-        name: 'Corn Plant',
-        imageUrl: 'https://i.ibb.co/bPmVXyP/black-converse.png',
-        price: 11
+        price: 11,
+        name: 'Corn Plant'
       },
       {
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fbathroom%2Firon-cross-begonia.jpg?alt=media&token=0c53c520-3d5f-43b3-ab08-99288b5a159b',
         id: 13,
-        name: 'Iron Cross Begonia',
-        imageUrl: 'https://i.ibb.co/1RcFPk0/white-nike-high-tops.png',
-        price: 18
+        price: 18,
+        name: 'Iron Cross Begonia'
       },
       {
+        price: 16,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fbathroom%2Fmaidenhair-fern.jpg?alt=media&token=bd8fc671-e078-4408-9978-c6b727f3b168',
         id: 14,
-        name: 'Maidenhair Fern',
-        imageUrl: 'https://i.ibb.co/QcvzydB/nikes-red.png',
-        price: 16
+        name: 'Maidenhair Fern'
       },
       {
         id: 15,
         name: 'Monstera Adansonii',
-        imageUrl: 'https://i.ibb.co/fMTV342/nike-brown.png',
-        price: 24
+        price: 24,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fbathroom%2Fmonstera-adansonii.jpg?alt=media&token=dc2e2fc7-9c34-4256-b3a9-87179f57c839'
       },
       {
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fbathroom%2Fsansivieria-moonshine.jpg?alt=media&token=ed51b0e8-ecdb-49f2-9191-bfa2fd0d40cc',
         id: 16,
         name: 'Sansivieria Moonshine',
-        imageUrl: 'https://i.ibb.co/w4k6Ws9/nike-funky.png',
         price: 19
       },
       {
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fbathroom%2Fschefflera.jpg?alt=media&token=c8be5150-3d7a-4054-90f7-511da1457773',
         id: 17,
         name: 'Schefflera',
-        imageUrl: 'https://i.ibb.co/Mhh6wBg/timberlands.png',
         price: 20
       }
     ]
   },
-  living: {
-    id: 3,
-    title: 'Living Room',
-    routeName: 'living',
+  bedroom: {
+    routeName: 'bedroom',
+    id: 'fqbI8kN5rM5ZsA9DN7fz',
+    title: 'Bedroom',
     items: [
       {
-        id: 18,
-        name: 'Fiddle Leaf Fig',
-        imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
-        price: 25
+        name: 'Aloe Vera',
+        price: 25,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fbedroom%2Faloe-vera.jpg?alt=media&token=e16d8c23-246c-4db9-8829-60977fa32235',
+        id: 1
       },
       {
-        id: 19,
-        name: 'Kentia Palm',
-        imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
-        price: 35
+        price: 18,
+        name: 'Calathea',
+        id: 2,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fbedroom%2Fcalathea.jpg?alt=media&token=62ae9b33-05e8-4d33-8111-f08d0eb5353f'
       },
       {
-        id: 20,
-        name: 'Parlour Palm',
-        imageUrl: 'https://i.ibb.co/N71k1ML/grey-jean-jacket.png',
-        price: 23
+        name: 'Chinese Money Plant',
+        price: 35,
+        id: 3,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fbedroom%2Fchinese-money-plant.jpg?alt=media&token=8b1b0c2b-6526-41d1-ae70-0487c6ec3d5e'
       },
       {
-        id: 21,
-        name: 'Rubber Plant',
-        imageUrl: 'https://i.ibb.co/s96FpdP/brown-shearling.png',
-        price: 64
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fbedroom%2Fdevils-ivy.jpg?alt=media&token=27353a34-d65b-464c-a4d2-244e91fc95b4',
+        price: 25,
+        name: 'Devils Ivy',
+        id: 4
       },
       {
-        id: 22,
-        name: 'Swiss Cheese Plant',
-        imageUrl: 'https://i.ibb.co/M6hHc3F/brown-trench.png',
-        price: 54
-      }
-    ]
-  },
-  kitchen: {
-    id: 4,
-    title: 'Kitchen',
-    routeName: 'kitchen',
-    items: [
-      {
-        id: 23,
-        name: 'Aspidistra',
-        imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
-        price: 25
+        id: 5,
+        price: 18,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fbedroom%2Fpeace-lily.jpg?alt=media&token=14865d8a-85ab-44c2-913b-6cdf8bf53fa0',
+        name: 'Peace Lily'
       },
       {
-        id: 24,
-        name: 'Dracena Fragrans',
-        imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
-        price: 20
+        price: 14,
+        id: 6,
+        name: 'Snake Plant',
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fbedroom%2Fsnake-plant.webp?alt=media&token=a3419903-177f-45a4-b20b-f7a5b87bd547'
       },
       {
-        id: 25,
-        name: 'English Ivy',
-        imageUrl: 'https://i.ibb.co/KV18Ysr/floral-skirt.png',
-        price: 40
+        price: 18,
+        id: 7,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fbedroom%2Fsucculent.jpg?alt=media&token=d84e1354-8a20-4db2-95be-f690750a91b4',
+        name: 'Succulent'
       },
       {
-        id: 26,
-        name: 'Phlebodium Aureum',
-        imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
-        price: 40
+        price: 14,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fbedroom%2Fwatermelon-peperomia.jpg?alt=media&token=91636aa8-c5c1-4b40-9f94-d7c8a7e40dfc',
+        name: 'Watermelon Peperomia',
+        id: 8
       },
       {
-        id: 27,
-        name: 'Satin Pothos',
-        imageUrl: 'https://i.ibb.co/KmSkMbH/striped-sweater.png',
-        price: 45
-      },
-      {
-        id: 28,
-        name: 'Strelitzia Nicolai',
-        imageUrl: 'https://i.ibb.co/v1cvwNf/yellow-track-suit.png',
-        price: 65
-      },
-      {
-        id: 29,
-        name: 'Tradescantia Sitara',
-        imageUrl: 'https://i.ibb.co/qBcrsJg/white-vest.png',
-        price: 45
+        price: 16,
+        name: 'Weeping Fig',
+        id: 9,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fbedroom%2Fweeping-fig.jpg?alt=media&token=67a27f5b-d862-41dd-bf5f-355f2efa426f'
       }
     ]
   },
   study: {
-    id: 5,
-    title: 'Study',
     routeName: 'study',
+    id: 'jhVFSr5vwRtQR1pb5pyh',
+    title: 'Study',
     items: [
       {
-        id: 30,
         name: 'Alocasia Gageana',
-        imageUrl: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
+        id: 30,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fstudy%2Falocasia-gageana.jpg?alt=media&token=6407c9d8-3981-4ba6-8c7a-ed552ffa9329',
         price: 32
       },
       {
-        id: 31,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fstudy%2Falocasia-zebrina.jpg?alt=media&token=6650f79c-904f-4ead-aee2-eede1a1c087f',
+        price: 20,
         name: 'Alocasia Zebrina',
-        imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
-        price: 20
+        id: 31
       },
       {
-        id: 32,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fstudy%2Fasparagus-fern.jpg?alt=media&token=db05be65-f9be-4737-9d64-797b825e16cb',
         name: 'Asparagus Fern',
-        imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
-        price: 25
+        price: 25,
+        id: 32
       },
       {
-        id: 33,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fstudy%2Fceropegia-woodii.jpg?alt=media&token=c9bad500-c19a-4067-ba6e-f260c353c0d6',
         name: 'Ceropegia Woodlii',
-        imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
-        price: 25
+        price: 25,
+        id: 33
       },
       {
         id: 34,
-        name: 'Euphorbia Abyssinica',
-        imageUrl: 'https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png',
-        price: 40
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fstudy%2Feuphorbia-abyssinica.jpg?alt=media&token=e63e4fc5-6079-475d-94e3-5168ccacdc5c',
+        price: 40,
+        name: 'Euphorbia Abyssinica'
       },
       {
         id: 35,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fstudy%2Fficus-benjamina.jpg?alt=media&token=b635ea87-3697-4556-947f-59a1db5bb2b2',
         name: 'Ficus Benjamina',
-        imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
         price: 25
       },
       {
-        id: 36,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fstudy%2Fphilodendron-scandens.jpg?alt=media&token=92dfdbec-422a-4f88-b788-055ff67f78ea',
+        price: 45,
         name: 'Philodendron Scandens',
-        imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
-        price: 45
+        id: 36
       },
       {
         id: 37,
-        name: 'Sweetheart Plant',
-        imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
-        price: 35
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fstudy%2Fsweetheart-plant.jpg?alt=media&token=32068ca9-7009-4dbc-99bf-36f8ac3073f1',
+        price: 35,
+        name: 'Sweetheart Plant'
+      }
+    ]
+  },
+  kitchen: {
+    routeName: 'kitchen',
+    id: 'oIynvEcqBvDMcxBjsUBf',
+    title: 'Kitchen',
+    items: [
+      {
+        name: 'Aspidistra',
+        id: 23,
+        price: 25,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fkitchen%2Faspidistra.jpg?alt=media&token=6df64189-dd25-4019-ba07-1134b8f560b2'
+      },
+      {
+        name: 'Dracena Fragrans',
+        id: 24,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fkitchen%2Fdracena-fragrans.jpg?alt=media&token=9f794eb6-0d0d-44ea-81d2-2de99eead315',
+        price: 20
+      },
+      {
+        id: 25,
+        price: 40,
+        name: 'English Ivy',
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fkitchen%2Fenglish-ivy.jpg?alt=media&token=8f6d7ccb-0d72-4da2-aa9d-475b4288989b'
+      },
+      {
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fkitchen%2Fphlebodium-aureum.jpg?alt=media&token=6cfdad60-6c8d-403d-9853-ecbd895fd64a',
+        name: 'Phlebodium Aureum',
+        price: 40,
+        id: 26
+      },
+      {
+        price: 45,
+        name: 'Satin Pothos',
+        id: 27,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fkitchen%2Fsatin-pothos.jpg?alt=media&token=9c2961ae-1d4b-4452-87a5-b432b1007d01'
+      },
+      {
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fkitchen%2Fstrelitzia-nicolai.jpg?alt=media&token=0a645de6-9675-4eab-b6de-79639af30de1',
+        name: 'Strelitzia Nicolai',
+        id: 28,
+        price: 65
+      },
+      {
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fkitchen%2Ftradescantia-sitara.jpg?alt=media&token=841f32b5-6d9b-40d8-a0be-9522507d4f42',
+        price: 45,
+        id: 29,
+        name: 'Tradescantia Sitara'
+      }
+    ]
+  },
+  'living room': {
+    routeName: 'living%20room',
+    id: 'pO6H9aqhjsFwL30QE1m3',
+    title: 'Living Room',
+    items: [
+      {
+        name: 'Fiddle Leaf Fig',
+        id: 18,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fliving%2Ffiddle-leaf-fig.jpg?alt=media&token=02c8385e-feec-4ab5-b09a-1755f0db2deb',
+        price: 25
+      },
+      {
+        price: 35,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fliving%2Fkentia-palm.jpg?alt=media&token=ced21f21-5d82-4612-aef3-c5b4d8828636',
+        name: 'Kentia Palm',
+        id: 19
+      },
+      {
+        id: 20,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fliving%2Fparlour-palm.jpg?alt=media&token=08d90a17-9b9f-4bba-bda0-9fcbc7df6196',
+        name: 'Parlour Palm',
+        price: 23
+      },
+      {
+        id: 21,
+        price: 64,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fliving%2Frubber-plant.jpg?alt=media&token=58fce95f-4879-4118-b0ee-d95bce0db70d',
+        name: 'Rubber Plant'
+      },
+      {
+        name: 'Swiss Cheese Plant',
+        price: 54,
+        id: 22,
+        imageUrl:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-7fec4.appspot.com/o/plants%2Fliving%2Fswiss-cheese-plant.jpg?alt=media&token=0fecb0df-ef76-4c8c-ba9e-83fd5eb2f6e6'
       }
     ]
   }
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,22 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './root-reducer';
-import rootSaga from './root-saga';
 
-const sagaMiddleware = createSagaMiddleware();
-
-const middleWares = [sagaMiddleware];
+const middleWares = [];
 
 if (process.env.NODE_ENV === 'development') {
   middleWares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middleWares));
-
-sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
