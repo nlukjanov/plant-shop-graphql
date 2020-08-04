97f82ef3ef50d2a968a956b6f466f4c6867fd9df
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-08-04 10:03:12 +0100

    fix resolver cache destructuring

--- client/src/graphql/resolvers.js
+++ client/src/graphql/resolvers.js
@@ -13,7 +13,7 @@ const GET_CART_HIDDEN = gql`
 `;
 export const resolvers = {
   Mutation: {
-    toggleCartHidden: (_root, _args, _context, _info) => {
+    toggleCartHidden: (_root, _args, { cache }) => {
       const { cartHidden } = cache.readQuery({
         query: GET_CART_HIDDEN
       });
