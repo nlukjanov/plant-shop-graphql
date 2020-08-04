d248e152b21c91fde87cb93bf645613016a657de
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-08-04 09:27:31 +0100

    add cartHidden Mutation on the server

---
+++ client/src/graphql/resolvers.js
@@ -0,0 +1,29 @@
+import { gql } from 'apollo-boost';
+
+export const typeDefs = gql`
+  extend type Mutation {
+    toggleCartHidden: Boolean!
+  }
+`;
+
+const GET_CART_HIDDEN = gql`
+  {
+    cartHidden @client
+  }
+`;
+export const resolvers = {
+  Mutation: {
+    toggleCartHidden: (_root, _args, _context, _info) => {
+      const { cartHidden } = cache.readQuery({
+        query: GET_CART_HIDDEN
+      });
+
+      cache.writeQuery({
+        query: GET_CART_HIDDEN,
+        data: { cartHidden: !cartHidden }
+      });
+
+      return !cartHidden;
+    }
+  }
+};
